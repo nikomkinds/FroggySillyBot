737a753207c157d907a0320df6877dd41b26f99b
str1xoid <134718130+str1xoid@users.noreply.github.com>
2025-05-29 13:48:42 +0300

    tracking go.mod

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,1 +1,43 @@
-package bot
+package main
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"github.com/nikomkinds/FroggySillyBot/internal/handlers"
+	"log"
+	"os"
+)
+
+func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+	token := os.Getenv("BOT_TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	updates := bot.GetUpdatesChan(u)
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		if update.Message.IsCommand() {
+			switch update.Message.Command() {
+			case "help":
+				handlers.HandleHelpCommand(bot, update)
+			default:
+				bot.Send(tgbotapi.NewMessage(
+					update.Message.Chat.ID,
+					"Unknown command...",
+				))
+			}
+		}
+	}
+}
--- internal/handlers/help.go
+++ internal/handlers/help.go
@@ -1,1 +1,16 @@
 package handlers
+
+import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+func HandleHelpCommand(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+
+	helpMessage := `
+Hello!
+Available commands:
+/help - for help`
+
+	bot.Send(tgbotapi.NewMessage(
+		update.Message.Chat.ID,
+		helpMessage,
+	))
+}
